b683c0527a099957b876d5a16e0398291b6558d3
James <jameshaller27@gmail.com>
2021-10-18 13:49:03 -0700

    Update SmelterNode to use MachineNode base

--- scripts/SmelterNode.cs
+++ scripts/SmelterNode.cs
@@ -1,13 +1,10 @@
 using Godot;
 using FactoryPlanner.scripts.machines;
 using FactoryPlanner.scripts.resources;
-using Resource = FactoryPlanner.scripts.resources.Resource;
 
 public class SmelterNode : MachineNode
 {
     private OptionButton ResourceOptionButton => this.GetChild<HBoxContainer>(0).GetChild<VBoxContainer>(1).GetChild<OptionButton>(2);
-    private Label InputLabel => this.GetChild<HBoxContainer>(0).GetChild<VBoxContainer>(0).GetChild<Label>(0);
-    private Label OutputLabel => this.GetChild<HBoxContainer>(0).GetChild<VBoxContainer>(2).GetChild<Label>(0);
 
     internal SmelterNode() : base(1, 1)
     {
@@ -16,6 +13,8 @@ public class SmelterNode : MachineNode
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        base._Ready();
+
         AddEnumItems(this.ResourceOptionButton, typeof(ResourceList));
 
         this._on_Resource_Selected(0);
@@ -24,9 +23,8 @@ public class SmelterNode : MachineNode
     private void _on_Resource_Selected(int index)
     {
         ResourceList resourceEnumVal = (ResourceList)this.ResourceOptionButton.GetItemMetadata(index);
-        if (!Resource.TryGetResource(resourceEnumVal, out Resource fromMeta)) return;
+        this.Outputs[0].SetResource(resourceEnumVal);
 
-        this.SetSlot(0, this.IsSlotEnabledLeft(0), this.GetSlotTypeLeft(0), Colors.White, true, fromMeta.Id, fromMeta.Color);
-        this.OutputLabel.Text = fromMeta.Name;
+        this.UpdateSlots();
     }
 }
