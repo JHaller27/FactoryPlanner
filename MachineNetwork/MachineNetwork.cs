0c493321bca88dfbfe0be7e7eb3124359938d25c
James <jameshaller27@gmail.com>
2022-04-27 13:10:41 -0700

    Clean-up list of all machines

--- MachineNetwork/MachineNetwork.cs
+++ MachineNetwork/MachineNetwork.cs
@@ -6,13 +6,12 @@ namespace MachineNetwork
 {
     public class MachineNetwork
     {
-        private const int RetryCount = 16;
+        private const int RetryCount = 1024;
 
         private static MachineNetwork _instance;
         public static MachineNetwork Instance => _instance ??= new();
 
-        private Dictionary<int, MachineBase> RandomAccessList { get; } = new();
-        private Dictionary<MachineBase, int> ReverseRandomAccessList { get; } = new();
+        private HashSet<MachineBase> AllMachines { get; } = new();
 
         private HashSet<MachineBase> Roots { get; } = new();
         private HashSet<MachineBase> Leaves { get; } = new();
@@ -21,17 +20,13 @@ namespace MachineNetwork
         {
         }
 
-        public int AddMachine(MachineBase machine)
+        public void AddMachine(MachineBase machine)
         {
-            int key = this.RandomAccessList.Count;
-            this.RandomAccessList.Add(key, machine);
-            this.ReverseRandomAccessList.Add(machine, key);
+            this.AllMachines.Add(machine);
             this.Roots.Add(machine);
             this.Leaves.Add(machine);
 
             this.Recalculate();
-
-            return key;
         }
 
         public void RemoveMachine(MachineBase machineBase)
@@ -45,23 +40,12 @@ namespace MachineNetwork
                 this.DisconnectMachines(machineBase, neighbor);
             }
 
-            int key = this.ReverseRandomAccessList[machineBase];
-            this.ReverseRandomAccessList.Remove(machineBase);
-            this.RandomAccessList.Remove(key);
+            this.AllMachines.Remove(machineBase);
 
             this.Roots.Remove(machineBase);
             this.Leaves.Remove(machineBase);
 
             this.Recalculate();
-        }
-
-        public IEnumerable<MachineBase> GetMachines() => this.RandomAccessList.Values;
-        public IEnumerable<MachineBase> GetRoots() => this.Roots;
-        public IEnumerable<MachineBase> GetLeaves() => this.Leaves;
-
-        private MachineBase GetMachine(int idx)
-        {
-            return this.RandomAccessList[idx];
         }
 
         public void ConnectMachines(MachineBase from, int fromSlot, MachineBase to, int toSlot)
@@ -72,11 +56,6 @@ namespace MachineNetwork
             this.Roots.Remove(to);
 
             this.Recalculate();
-        }
-
-        public void ConnectMachines(int fromId, int fromSlot, int toId, int toSlot)
-        {
-            this.ConnectMachines(this.GetMachine(fromId), fromSlot, this.GetMachine(toId), toSlot);
         }
 
         private void DisconnectMachines(MachineBase from, MachineBase to)
@@ -97,11 +76,6 @@ namespace MachineNetwork
             this.DisconnectMachines(from, to);
 
             this.Recalculate();
-        }
-
-        public void DisconnectMachines(int fromId, int fromSlot, int toId, int toSlot)
-        {
-            this.DisconnectMachines(this.GetMachine(fromId), fromSlot, this.GetMachine(toId), toSlot);
         }
 
         private static IEnumerable<MachineBase> Traverse(IEnumerable<MachineBase> start)
